cdb3a89eac5879f2938f5b87ce2cd82f72e9ed1b
zjyl1994 <me@zjyl1994.com>
2025-06-11 23:46:21 +0800

    feat: finish catch

--- service/catchgame/action/catch.go
+++ service/catchgame/action/catch.go
@@ -1,15 +1,27 @@
 package action
 
 import (
+	"fmt"
+	"math/rand/v2"
+	"strconv"
+	"strings"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/zjyl1994/yusifubot/infra/utils"
 	"github.com/zjyl1994/yusifubot/infra/vars"
 	"github.com/zjyl1994/yusifubot/service/catchgame/catchobj"
+	"github.com/zjyl1994/yusifubot/service/catchgame/catchret"
 	"github.com/zjyl1994/yusifubot/service/catchgame/common"
 	"github.com/zjyl1994/yusifubot/service/catchgame/stamina"
 )
 
 func CatchHandler(msg *tgbotapi.Message) (err error) {
+	user := common.UserRel{
+		ChatId: msg.Chat.ID,
+		UserId: msg.From.ID,
+	}
+	catchLock.Lock(user)
+	defer catchLock.Unlock(user)
 	// 获取捕捉对象
 	var catchObjList []catchobj.CatchObj
 	if msg.ReplyToMessage != nil { // 捕捉特定用户
@@ -28,10 +40,6 @@ func CatchHandler(msg *tgbotapi.Message) (err error) {
 		}
 	}
 	// 计算捕捉数量
-	user := common.UserRel{
-		ChatId: msg.Chat.ID,
-		UserId: msg.From.ID,
-	}
 	var catchNum int64
 	switch msg.Command() {
 	case "catch":
@@ -47,11 +55,51 @@ func CatchHandler(msg *tgbotapi.Message) (err error) {
 		}
 		catchNum = sp.Current() / CATCH_STAMINA_COST
 	}
-	// 抓
-	sp, err := stamina.UseStaminPoint(vars.DBInstance, user, catchNum*CATCH_STAMINA_COST)
+	// 消耗体力
+	_, err = stamina.UseStaminPoint(vars.DBInstance, user, catchNum*CATCH_STAMINA_COST)
 	if err != nil {
 		return err
 	}
-	// TODO: finish here
-	return nil
+	// 捕捉
+	catchResult := make([]catchobj.CatchObj, len(catchObjList))
+	catchCount := make(map[catchobj.CatchObj]int64)
+	var successCtr int64
+	var emojiResult string
+	for i := 0; i < len(catchObjList); i++ {
+		// 选择捕捉对象
+		choiceObj := catchObjList[rand.IntN(len(catchObjList))]
+		// 计算是否成功
+		success := rand.Float64() < CATCH_RATE
+		// 记录成功内容
+		if success {
+			catchResult[i] = choiceObj
+			catchCount[choiceObj]++
+			successCtr++
+
+			if choiceObj.Emoji != "" {
+				emojiResult += choiceObj.Emoji
+			} else {
+				emojiResult += CATCH_DEFAULT_EMOJI
+			}
+		} else {
+			emojiResult += CATCH_MISS_EMOJI
+		}
+	}
+	var sb strings.Builder
+	sb.WriteString("捕捉结果\n\n")
+	sb.WriteString(emojiResult)
+	sb.WriteString("\n\n成功率：")
+	sb.WriteString(fmt.Sprintf("%.2f%%\n\n\n\n", float64(successCtr)/float64(catchNum)*100))
+	for _, obj := range catchResult {
+		err = catchret.GiveCatchNum(vars.DBInstance, user, obj.ID, catchCount[obj])
+		if err != nil {
+			return err
+		}
+		sb.WriteString(obj.Name)
+		sb.WriteString(" ")
+		sb.WriteString(strconv.FormatInt(catchCount[obj], 10))
+		sb.WriteString("只\n\n")
+	}
+
+	return utils.ReplyTextToTelegram(msg, sb.String(), true)
 }
--- service/catchgame/action/define.go
+++ service/catchgame/action/define.go
@@ -1,7 +1,17 @@
 package action
 
+import (
+	"github.com/zjyl1994/yusifubot/infra/utils/kmutex"
+	"github.com/zjyl1994/yusifubot/service/catchgame/common"
+)
+
 const (
 	CATCH_MISS_EMOJI    = "✨️" // 未抓到emoji
 	CATCH_DEFAULT_EMOJI = "👀"  // 抓到但未设置时的默认emoji
 	CATCH_STAMINA_COST  = 10   // 一次抓的SP消耗
+	CATCH_RATE          = 0.4  // 抓中率
+)
+
+var (
+	catchLock = kmutex.NewKmutex(common.UserRelHasher, 100) //并发捕捉锁
 )
